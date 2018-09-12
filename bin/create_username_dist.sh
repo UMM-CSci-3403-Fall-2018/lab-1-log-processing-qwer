496bbb4698503c8aecbd3dfa27799edc40610275
Dexter <plum_style@localhost.localdomain>
2018-09-12 01:38:00 -0500

    updated the create_username_dist.sh still has problem remain

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,4 +2,12 @@
 
 DIRECTORY=$1
 
+HERE=$(pwd)
 
+cd $DIRECTORY
+
+sort ./*/failed_login_data.txt | uniq -c | awk '{print "data.addRow([\x27" $5 "\x27, " $1 "]);"}' > temp.html
+
+cat $HERE/html_components/username_dist_header.html temp.html $HERE/html_components/username_dist_footer.html > username_dist.html
+
+rm temp.html
